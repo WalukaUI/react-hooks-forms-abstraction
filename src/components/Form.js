982a4a074b3dc2bcb60c92aa923ffab7310176d6
WalukaUI <cwaluka@yahoo.com>
2021-05-18 22:30:40 -0700

    code along

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,24 +1,50 @@
 import React, { useState } from "react";
 
 function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    birthYear: "",
+    admin: false,
+  });
 
-  function handleFirstNameChange(event) {
-    setFirstName(event.target.value);
+  // function handleFirstNameChange(event) {
+  //   setFormData({...formData,
+  //     firstName:event.target.value})
+  // }
+
+  // function handleLastNameChange(event) {
+  //   setFormData({...formData,
+  //     lastName:event.target.value})
+  // }
+
+  function handleChange(event) {
+    // name is the KEY in of the formData object we're trying to update
+    const name = event.target.name;
+    let value = event.target.value;
+    // use `checked` property of checkboxes instead of `value`
+    if (event.target.type === "checkbox") {
+      value = event.target.checked;
+    }
+
+      setFormData({
+        ...formData,
+        [name]: value,
+      });
+    }
+    function handleSubmit(event) {
+      event.preventDefault();
+      console.log(formData);
+    }
+    return (
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="firstName" onChange={handleChange} placeholder="First name..." value={formData.firstName} />
+        <input type="text" name="lastName" onChange={handleChange} placeholder="Last name..." value={formData.lastName} />
+        <input type="number" name="birthYear" onChange={handleChange} placeholder="Birth year..." value={formData.birthYear} min="1900" max="2021" />
+        <input type="checkbox" name="admin" onChange={handleChange} checked={formData.admin} /><label>Check Box</label>
+        <button type="submit">Submit</button>
+      </form>
+    );
   }
 
-  function handleLastNameChange(event) {
-    setLastName(event.target.value);
-  }
-
-  return (
-    <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
-      <button type="submit">Submit</button>
-    </form>
-  );
-}
-
-export default Form;
+  export default Form
